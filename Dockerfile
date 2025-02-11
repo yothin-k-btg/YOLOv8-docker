6e18c79e24c2107712c4f4ec1d9a19aa2b4d951f
yothin-k-btg <YothinK@betagro.com>
2025-02-11 10:20:35 +0700

    first yolo run

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,23 @@
 
 FROM ultralytics/ultralytics:latest
 
-ENV APP_HOME /home/app
+ENV APP_HOME=/home/app
 WORKDIR ${APP_HOME}
 
 # Copy the requirements file and install dependencies
 COPY ./requirements.txt ${APP_HOME}/requirements.txt
-RUN pip install --no-cache-dir --upgrade -r ${APP_HOME}/requirements.txt
+RUN pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org --no-cache-dir --upgrade -r ${APP_HOME}/requirements.txt
+# RUN pip install --no-cache-dir --upgrade -r ${APP_HOME}/requirements.txt
+
 
 # Copy the app directory
 COPY ./app ${APP_HOME}/app
+COPY ./yolov8n_test.pt ${APP_HOME}/yolov8n_test.pt
 
+EXPOSE 8080
+
+# fastapi run app/main.py --port 8080
 CMD ["fastapi", "run", "app/main.py", "--port", "8080"]
+
+# uvicorn app.main:app --reload
+# CMD ["uvicorn", "app.main:app", "--reload"]
--- app/model/model.py
+++ app/model/model.py
@@ -5,10 +5,19 @@ from PIL import Image
 import ultralytics
 import ultralytics.engine
 import ultralytics.engine.results
+import albumentations as A
+import numpy as np
 
-__version__ = "1.0.0"
+__version__ = "0.1"
 
-model = ultralytics.YOLO('yolov8m.pt')
+# model = ultralytics.YOLO('yolov8n.pt')
+model = ultralytics.YOLO('yolov8n_test.pt')
+
+# setup transformer
+transformer = A.Compose([
+    A.CLAHE(always_apply=True)
+    
+], bbox_params=A.BboxParams(format='yolo'))
 
 def inference_on_path(imgs_path: str) -> List[Dict[str, Any]]:
     """
@@ -30,6 +39,11 @@ def inference_on_img(img: Image) -> List[Dict[str, Any]]:
     """
     Runs inference on the YOLOv8 architecture for the given image
     """
+    
+    image_np = np.array(img)
+    transformed = transformer(image=image_np)
+    img = Image.fromarray(transformed['image'])
+
     results: ultralytics.engine.results.Results = model(source=img, show=False, conf=0.45)
     result_data = json.loads(results[0].tojson())
 
